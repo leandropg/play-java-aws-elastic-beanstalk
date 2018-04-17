57b9331fcdd328afd5d7543df2191418c08f659e
Leandro Perez Guatibonza <leandropg@gmail.com>
2018-04-16 23:46:39 -0500

    Play Java AWS Elastic Beanstalk 1.0.0-SNAPSHOT - Add Evolutions Script

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,17 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table visit (
+  id                            integer auto_increment not null,
+  ip_address                    varchar(255),
+  visit_date                    datetime(6),
+  constraint pk_visit primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists visit;
+
